2e7ade2a0c670e2caad33f41190206b090a75551
yuridrifto <slowyuri@gmail.com>
2026-05-08 16:28:40 +0800

    add instagram reel project

---
+++ InstagramReelProgram/insta.c
@@ -0,0 +1,13 @@
+// Terminal UI ( ncruses )
+// Reel Feed Controller
+// HTTP/API Layer (libcurl)
+// JSON Parse (cJson/jansson)
+// Video Playback (mpv/ffmpeg)
+//
+
+#include <stdio.h>
+
+int main ()
+{
+    return 0;
+}
